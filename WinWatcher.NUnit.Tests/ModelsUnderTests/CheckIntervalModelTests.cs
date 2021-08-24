10a99cc280bdd0143e7caa6c17f1f3d5a2e5f647
Gosha94 <gerizch@rambler.ru>
2021-08-24 09:29:07 +0300

    add Intervals Model tests

--- WinWatcher.NUnit.Tests/ModelsUnderTests/CheckIntervalModelTests.cs
+++ WinWatcher.NUnit.Tests/ModelsUnderTests/CheckIntervalModelTests.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using WinWatcher.Models;
 
 namespace WinWatcher.NUnit.Tests
 {
-    class CheckIntervalModelTests
+    [TestFixture]
+    public class CheckIntervalModelTests
     {
+        private CheckIntervalModel  _checkIntervalModelUnderTests;
+        private IntervalDto         _testIntervalDto;
+
+        [SetUp]
+        public void StartBeforeEachTest()
+        { }
+
+        [Test]
+        [TestCase(10, 20)]
+        public void CalculatedIntervalDataTest_PassIfReturnsCorrectIntervalDtoModel(int testLifeTime, int testFrequency)
+        {
+            _checkIntervalModelUnderTests = new CheckIntervalModel(testLifeTime, testFrequency);
+            _checkIntervalModelUnderTests.GetCalculatedIntervalData();
+        }
     }
 }
